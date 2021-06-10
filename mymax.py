5d36e14dc95788529f01118ea5cdbc941217bf4f
Alexander Kilinkarov <linxam@gmail.com>
2021-06-11 00:59:22 +0300

    Initial commit. mymax.py file added

---
+++ mymax.py
@@ -0,0 +1,11 @@
+def mymax(l):
+    '''Custom max function. Python example/exercise.''' 
+    m = l[0]
+    for i in l:
+        if i>m:
+            m = i
+    return m
+    
+print(mymax([2, 5, 3, 1]))
+print(mymax('zxcvzxcvzxcvxz'))
+print(mymax(('asdf', 'qwer', 'zxcv')))

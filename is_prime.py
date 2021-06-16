59f08ce7bfe4d77eacbfec01a1109f7513e9fa0d
Alexander Kilinkarov <linxam@gmail.com>
2021-06-17 00:20:34 +0300

    is_prime.py updated

--- is_prime.py
+++ is_prime.py
@@ -1,6 +1,11 @@
 def is_prime(num: int) -> bool:
     '''this function returns True if there is a prime number, else returns False'''
-    for i in range(2, int(num**0.5)+1):
+    for i in range(2, round(num**0.5)+1):
         if num%i==0:
             return False
     return True
+
+if __name__=='__main__':
+    for i in range(20):
+        print(i, is_prime(i))
+
